c74a9e6941b288b7ccaba934140c1ec181756126
Ranbir-GNE <ranbir2115113@gndec.ac.in>
2024-02-25 16:12:03 +0530

    Update cse6.jsx

--- src/Components/Sidebar/Branches/ComputerScience/Semester6/cse6.jsx
+++ src/Components/Sidebar/Branches/ComputerScience/Semester6/cse6.jsx
@@ -5,16 +5,16 @@ import { Link } from 'react-router-dom';
 function cse6() {
     return (
         <div className="big-wrapper">
-            <Link to="https://example.com" className="small-wrapper">
+            <Link to="https://drive.google.com/drive/folders/1znPHJOgjU5DtVolDIr8_9sjR_Hvq6AvB?usp=sharing" className="small-wrapper">
                 <h3>Machine Learning</h3>
             </Link>
-            <Link to="https://example.com" className="small-wrapper">
+            <Link to="https://drive.google.com/drive/folders/1FScR_JyGAomnQW3W201aTLQU-eY4maw-?usp=sharing" className="small-wrapper">
                 <h3>Computer Craphics</h3>
             </Link>
-            <Link to="https://example.com" className="small-wrapper">
+            <Link to="https://drive.google.com/drive/folders/1O7-6EO7AXn12lzMoOvDmKiD6prVZlEtq?usp=sharing" className="small-wrapper">
                 <h3>Compiler Design</h3>
             </Link>
-            <Link to="https://example.com" className="small-wrapper">
+            <Link to="https://drive.google.com/drive/folders/1c_72Ow3nYazZZMwhtYnGjl-jxi0JE0et?usp=sharing" className="small-wrapper">
                 <h3>Cyber Security</h3>
             </Link>
             <Link to="https://example.com" className="small-wrapper">
