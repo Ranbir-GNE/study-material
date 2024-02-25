789ee5d09022f082e4a6ef8504b86d2df215e51a
Savy011 <savy.novachez011@proton.me>
2024-02-25 15:49:45 +0530

    fixed mobile styles

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,21 +7,22 @@ function Navbar() {
   return (
     <nav className="navbar">
       <ul>
-      <li><Link to="/dashboard">Dashboard</Link></li>
-            <li><Link to="/admin">Admin</Link></li>
-            <li><Link to="/student">Student</Link></li>
-            <li><Link to="/academics">Academics</Link></li>
-            <li><Link to="/fees">Fees Collection</Link></li>
-            <li><Link to="/examinations">Examinations</Link></li>
-            <li><Link to="/attendance">Attendance</Link></li>
-            <button><a href="https://curacare.vercel.app/" target="_blank" rel="noopener noreferrer">Communicate</a></button>
-            <li><Link to="/my-account">My Account</Link></li>
-            <li><Link to="/settings">Settings</Link></li>
-            <li><Link to="/change-password">Change Password</Link></li>
-            <li><Link to="/logout">Logout</Link></li>
+        <Link to="/dashboard"><li>Dashboard</li></Link>
+        <Link to="/admin"><li>Admin</li></Link>
+        <Link to="/student"><li>Student</li></Link>
+        <Link to="/academics"><li>Academics</li></Link>
+        <Link to="/fees"><li>Fees Collection</li></Link>
+        <Link to="/examinations"><li>Examinations</li></Link>
+        <Link to="/attendance"><li>Attendance</li></Link>
+        <a href="https://curacare.vercel.app/" target="_blank" rel="noopener noreferrer">
+          <li>Communicate</li></a>
+        <Link to="/my-account"><li>My Account</li></Link>
+        <Link to="/settings"><li>Settings</li></Link>
+        <Link to="/change-password"><li>Change Password</li></Link>
+        <Link to="/logout"><li>Logout</li></Link>
         {/* Add more links as needed */}
       </ul>
-    </nav>
+    </nav >
   );
 }
 
