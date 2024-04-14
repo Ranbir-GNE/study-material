200d6d64e719962027a3485f6553de260cfe8328
gundeeps247 <gundeepsinghm@gmail.com>
2024-04-14 15:35:30 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,28 @@ import Sidebar from './Components/Sidebar/Sidebar';
 import BranchTemplate from './Components/Branch';
 import CSc6 from './Components/Sidebar/Branches/ComputerScience/Semester6/cse6';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './Components/Sidebar/Sidebar.css'
+// import SubNavbar from './Components/Navbar/SubNavbar';
 import Navbar from './Components/Navbar/Navbar';
 
-function App() {
-  return (
-    <Router className="App">
-      <Navbar />
-      <Sidebar />
+function App() { 
+  return ( 
+    <Router>
+      <div>
+        <Navbar />
+        <div className="App">
+          {/* <SubNavbar /> */}
+          <Sidebar />
 
-      <Routes>
-        <Route path="/mechanical" element={<BranchTemplate branch="mechanical" linkSuffix="mech" />} />
-        <Route path="/civil" element={<BranchTemplate branch="civil" linkSuffix="civi" />} />
-        <Route path="/electrical" element={<BranchTemplate branch="electrical" linkSuffix="elec" />} />
-        <Route path="/computer-science" element={<BranchTemplate branch="computer-sciene" linkSuffix="cse" />} />
-        <Route path="/computer-science/:id" element={<CSc6 />} />
-        <Route path="/information-technology" element={<BranchTemplate branch="information-technology" linkSuffix="it" />} />
-      </Routes>
+          <Routes>
+            <Route path="/mechanical" element={<BranchTemplate branch="mechanical" linkSuffix="mech" />} />
+            <Route path="/civil" element={<BranchTemplate branch="civil" linkSuffix="civi" />} />
+            <Route path="/electrical" element={<BranchTemplate branch="electrical" linkSuffix="elec" />} />
+            <Route path="/computer-science" element={<BranchTemplate branch="computer-sciene" linkSuffix="cse" />} />
+            <Route path="/computer-science/:id" element={<CSc6 />} />
+            <Route path="/information-technology" element={<BranchTemplate branch="information-technology" linkSuffix="it" />} />
+          </Routes>
+        </div>
+      </div>
 
     </Router>
   );
